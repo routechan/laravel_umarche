f67521121c132a805b7e7b2a94494ebb268dc8d2
routechan <mr0326h14@gmail.com>
2024-09-11 13:23:41 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,5 +1,9 @@
+@props([
+    'title'=>'',
+    'message'=>'初期値だにょーん',
+'content'=>'本文の初期値'])
 <div class="border-2 shadow-md w-1/4 p-2">
-    <div{{$title}}</div>
+    <div>{{$title}}</div>
     <div>画像</div>
     <div>{{$content}}</div>
     <div>{{$message}}</div>
